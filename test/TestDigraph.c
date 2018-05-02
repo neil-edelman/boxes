76fa192923896a0bc30b4bf3555ab8cf7c596457
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2018-05-02 18:07:58 -0400

    Built example.

--- test/TestDigraph.c
+++ test/TestDigraph.c
@@ -82,35 +82,18 @@ static void E_filler(struct E *const e) {
 
 
 
+/* A more complex example using dynimic memory. */
 
-
-
-/* Regex. \url{ https://swtch.com/~rsc/regexp/regexp1.html }. */
-
-struct Regex;
-void Regex_(struct Regex **const pre);
-struct Regex *Regex(const char *const compile);
-const char *RegexMatch(const struct Regex *const re, const char *const match);
-int RegexOut(const struct Regex *const re, FILE *const fp);
-
-/** Intermediary structures created when walking the graph of regex and
- the remainder of the string that it's matched against. */
-struct Match {
-	const struct Transition *t;
-	const struct StateVertex *vertex;
-	const char *next;
-};
+struct Machine;
 
 /**
  * Transition virtual table, referenced from {Transition}. This forms the extra
  * data on the pointed to by the edges of the digraph.
  */
 struct Transition;
-typedef const char *(*Match)(const struct Match *const);
 typedef void (*TransitionToString)(const struct Transition *, char(*const)[12]);
 struct TransitionVt {
 	const char *debug;
-	const Match match;
 	const TransitionToString to_string;
 };
 
@@ -125,18 +108,12 @@ static void Transition(struct Transition *const t,
 	const struct TransitionVt *const vt) {
 	assert(t && vt);
 	t->vt = vt;
-	printf("Subconstructor Transition %s\n", vt->debug);
 }
 /** @implements TransitionToString */
 static void transition_to_string(const struct Transition *t,char(*const a)[12]){
 	t->vt->to_string(t, a);
 }
-/** @implements Match */
-static const char *transition_match(struct Match *const match) {
-	assert(match && match->t && match->vertex && match->next);
-	return match->t->vt->match(match);
-}
-#define DIGRAPH_NAME State
+#define DIGRAPH_NAME Machine
 #define DIGRAPH_EDATA struct Transition
 #define DIGRAPH_EDATA_TO_STRING &transition_to_string
 #include "../src/Digraph.h" /* StateDigraph, StateVertex, StateEdge. */
@@ -144,44 +121,65 @@ static const char *transition_match(struct Match *const match) {
 /*
  * {StateVertex} container.
  */
-#define POOL_NAME StateVertex
-#define POOL_TYPE struct StateVertex
-#define POOL_MIGRATE_ALL struct StateDigraph
+#define POOL_NAME Vertex
+#define POOL_TYPE struct MachineVertex
+#define POOL_MIGRATE_ALL struct Machine
 #include "Pool.h"
+
+/**
+ * {Empty} extends {Transition} as {StateEdgeLink}.
+ */
+/** @implements TransitionToString */
+static void empty_to_string(const struct Transition *e, char (*const a)[12]) {
+	sprintf(*a, "ε");
+	(void)e;
+}
+static struct TransitionVt empty_vt = { "Empty", empty_to_string };
+#define POOL_NAME Empty
+#define POOL_TYPE struct MachineEdgeLink
+#define POOL_MIGRATE_ALL struct Machine
+#include "Pool.h"
+/** Constructor.
+ @param e: This is instantiated; any data will be erased. */
+static struct MachineEdgeLink *Empty(struct EmptyPool *const ep) {
+	struct MachineEdgeLink *e;
+	assert(ep);
+	if(!(e = EmptyPoolNew(ep))) return 0;
+	Transition(&e->data.info, &empty_vt);
+	printf("Empty.\n");
+	return e;
+}
 
 /**
  * {Literals} extends {Transition} as {StateEdgeLink}.
  */
 struct Literals {
-	struct StateEdgeLink edge;
+	struct MachineEdgeLink edge;
 	char *text;
-	unsigned text_size;
+	size_t text_size;
 };
 /** {container_of} (probably does nothing.) */
-static const struct Literals *literals_holds_transition(
-	const struct Transition *const t) {
+static const struct Literals *literals_holds_transition(const struct
+	Transition *const t) {
 	return (const struct Literals *)(const void *)
-		((const char *)t
-		- offsetof(struct StateEdge, info)
-		- offsetof(struct StateEdgeLink, data)
-		- offsetof(struct Literals, edge));
-}
-/** @implements Match */
-static const char *literals_match(const struct Match *const match) {
-	const struct Literals *l = literals_holds_transition(match->t);
-	if(memcmp(l->text, match->next, l->text_size)) return 0;
-	return match->next + l->text_size;
+	((const char *)t
+	 - offsetof(struct MachineEdge, info)
+	 - offsetof(struct MachineEdgeLink, data)
+	 - offsetof(struct Literals, edge));
 }
 /** @implements TransitionToString */
 static void literals_to_string(const struct Transition *t, char (*const a)[12]){
 	sprintf(*a, "%.11s", literals_holds_transition(t)->text);
 }
-static struct TransitionVt literals_vt
-	= { "Literals", literals_match, literals_to_string };
-/** Destructor.
- @implements <Literals>Action */
+static struct TransitionVt literals_vt = { "Literals", literals_to_string };
+#define POOL_NAME Literals
+#define POOL_TYPE struct Literals
+#define POOL_MIGRATE_ALL struct Machine
+#include "Pool.h"
+/** Destructor because this takes up resources, but doesn't do anything about
+ the graph. */
 static void Literals_(struct Literals *const l) {
-	if(!l) return;
+	assert(l);
 	printf("~Literals: freeing %s.\n", l->text);
 	free(l->text);
 	l->text = 0;
@@ -190,162 +188,142 @@ static void Literals_(struct Literals *const l) {
 /** Constructor.
  @return Success.
  @throws {malloc} errors. */
-static int Literals(struct Literals *const l, const char *const match,
-	unsigned match_size) {
-	assert(l && match && match_size);
+static struct Literals *Literals(struct LiteralsPool *const lp,
+	const char *const text, size_t text_size) {
+	struct Literals *l;
+	assert(lp && text && text_size);
+	if(!(l = LiteralsPoolNew(lp))) return 0;
 	Transition(&l->edge.data.info, &literals_vt);
 	l->text = 0;
 	l->text_size = 0;
 	/* Copy the literals; null terminator even thought it's not really used. */
-	if(!(l->text = malloc(sizeof *match * (match_size + 1)))) return 0;
-	memcpy(l->text, match, match_size);
-	l->text[match_size] = '\0';
-	l->text_size = match_size;
-	printf("Literals %p: <%s>:%lu\n", (void *)l, l->text, (unsigned long)l->text_size);
-	return 1;
+	if(!(l->text = malloc(sizeof *text * (text_size + 1))))
+		{ LiteralsPoolPop(lp); return 0; }
+	memcpy(l->text, text, text_size);
+	l->text[text_size] = '\0';
+	l->text_size = text_size;
+	printf("Literals %p: <%s>:%lu\n", (void *)l, l->text,
+		(unsigned long)l->text_size);
+	return l;
 }
-#define POOL_NAME Literals
-#define POOL_TYPE struct Literals
-#define POOL_MIGRATE_ALL struct StateDigraph
-#include "Pool.h"
 
 /**
- * Regular expression contains all the above.
+ * Machine.
  */
-struct Regex {
-	int no;
-	struct StateDigraph states;
-	struct StateVertexPool vertices; /* Vertices are unadorned. */
+struct Machine {
+	const char *title;
+	struct MachineDigraph graph;
+	struct VertexPool v;
+	struct EmptyPool empties;
 	struct LiteralsPool literals;
 };
-/* Result of \see{regex_compile}. */
-enum CompileResult { CR_SUCCESS, CR_RESOURCES_FAIL, CR_SYNTAX_FAIL };
-/** Called from \see{Regex}. */
-static enum CompileResult regex_compile(struct Regex *re,
-	const char *const match) {
-	struct StateVertex *start, *end;
-	struct Literals *lit;
-	const char *m = match;
-	unsigned p = 0;
-	int done = 0;
-	while(*m) {
-		p++, m++;
-	}
-	printf("compile: <%s> upto: %u.\n", match, p);
-	do {
-		if(!(start = StateVertexPoolNew(&re->vertices))) break;
-		StateDigraphVertex(&re->states, start);
-		if(!(end = StateVertexPoolNew(&re->vertices))) break;
-		StateDigraphVertex(&re->states, end);
-		if(!(lit = LiteralsPoolNew(&re->literals)) || !Literals(lit, match, p))
-			break;
-		StateDigraphEdge(&lit->edge.data, start, end);
-		done = 1;
-	} while(0); if(!done) return CR_RESOURCES_FAIL;
-	return CR_SUCCESS;
-}
-/** Destructor. */
-void Regex_(struct Regex **const pre) {
-	struct Regex *re;
-	if(!pre || !(re = *pre)) return;
-	printf("~Regex: releasing #%d.\n", re->no);
-	LiteralsPoolForEach(&re->literals, &Literals_);
-	LiteralsPool_(&re->literals);
-	StateVertexPool_(&re->vertices);
-	StateDigraph_(&re->states);
-}
-/** Constructor. */
-struct Regex *Regex(const char *const compile) {
-	struct Regex *re;
-	enum CompileResult cr;
-	static int no;
-	if(!compile) return 0;
-	if(!(re = malloc(sizeof *re))) return 0;
-	re->no = ++no;
-	StateDigraph(&re->states);
-	StateVertexPool(&re->vertices, &StateDigraphVertexMigrateAll, &re->states);
-	LiteralsPool(&re->literals, &StateDigraphEdgeMigrateAll, &re->states);
-	printf("Compiling %d: <%s>.\n", re->no, compile);
-	cr = regex_compile(re, compile);
-	switch(cr) {
-		case CR_SUCCESS: break;
-		case CR_SYNTAX_FAIL: errno = EILSEQ;
-		case CR_RESOURCES_FAIL: Regex_(&re);
-	}
-	return re;
-}
-/** Checks the outbound edges from {m->vertex} (part of a compiled DFA) for a
- match on the string {m->next}.
- @return The end of the match if it was a match or null.
- @fixme implements <State,char*>Predicate,
- if(!(e = StateEdgeListMatchShortCircuit(&s->out, &no_match, match))) would be
- nice. List doesn't have interfaces . . . yet? */
-static int match_here(const struct StateVertex *const root, const char *const arg) {
-	struct Match m;
-	struct StateEdge *e;
-	const char *end;
-	enum { NOT_MATCHED, MATCHED } status = MATCHED;
-	m.t = 0, m.vertex = root, m.next = arg;
-	while(m.vertex) {
-		/* Finished when the vertex has degree zero. */
-		if(!(e = StateEdgeListFirst(&m.vertex->out))) break;
-		status = NOT_MATCHED;
-		do { /* Loop through all the edges out of this vertex. */
-			m.t = &e->info;
-			if(!(end = transition_match(&m))) continue;
-			m.vertex = e->to, m.next = end, status = MATCHED;
-			break;
-		} while((e = StateEdgeListNext(e)));
-		if(status == NOT_MATCHED) break;
-	}
-	return status == MATCHED ? 1 : 0;
-}
-/** Match {re}.
- @return The first point it matches or null if it doesn't. */
-const char *RegexMatch(const struct Regex *const re, const char *const arg) {
-	const struct StateVertex *root;
-	const char *a;
-	if(!re || !arg) return 0;
-	root = StateDigraphGetRoot(&re->states);
-	for(a = arg; *a; a++) if(match_here(root, a)) return a;
-	return 0;
-}
-/** Appends {re} to {fp} in GraphViz format.
- @param re: If null, does nothing.
- @param fp: File pointer.
- @return Success.
- @throws {fprintf} errors: {IEEE Std 1003.1-2001}.
- @order O(|{vertices}| + |{edges}|)
- @allow */
-int RegexOut(const struct Regex *const re, FILE *const fp) {
-	if(!re) return 0;
-	return StateDigraphOut(&re->states, fp);
+
+/*
+ * Temporary nesting for compiling.
+ * Refers to index in the vertices pool.
+ */
+#define POOL_NAME Nest
+#define POOL_TYPE size_t
+#define POOL_STACK
+#include "Pool.h"
+static size_t *Nest(struct NestPool *const np, const size_t idx) {
+	size_t *i;
+	assert(np);
+	if(!(i = NestPoolNew(np))) return 0;
+	*i = idx;
+	return i;
 }
 
-/** @return Either EXIT_SUCCESS or EXIT_FAILURE. */
+/** Called from \see{Regex}.
+ @return Success, otherwise {errno} will (probably) be set. */
+static int m_compile(struct Machine *m, const char *const compile) {
+	struct NestPool np;
+	size_t *n;
+	struct MachineVertex *v;
+	const char *c = compile, *start = 0;
+	int is_done = 0, is_out = 0;
+	enum { SUCCESS, RESOURCES, SYNTAX } e = SUCCESS;
+
+	NestPool(&np);
+	printf("compile: <%s>.\n", compile);
+	do { /* try */
+		if(!(n = NestPoolNew(&np)) || !(v = VertexPoolNew(&m->v))
+		   || Nest(&np, VertexPoolIndex(&m->v, v))) break;
+		MachineDigraphVertex(&m->graph, v);
+		do {
+			switch(*c) {
+				case '\0': is_done = is_out = 1; break;
+				default: if(!start) start = c; break;
+			}
+			if(is_out) {
+				struct MachineEdge *edge;
+				struct MachineVertex *v1 = VertexPoolNew(&m->v);
+				if(!v1) { e = RESOURCES; break; }
+				MachineDigraphVertex(&m->graph, v);
+				is_out = 0;
+				if(start && start < c - 1) {
+					struct Literals *lit;
+					if(!(lit = Literals(&m->literals, start, c - 1 - start)))
+						{ e = RESOURCES; break; }
+					edge = &lit->edge.data;
+				} else {
+					struct MachineEdgeLink *emp;
+					if(!(emp = Empty(&m->empties)))
+						{ e = RESOURCES; break; }
+					edge = &emp->data;
+				}
+				start = 0;
+				MachineDigraphEdge(edge, v, v1);
+			}
+		} while(c++, !is_done);
+		if(e) break;
+	} while(0); if(e == SYNTAX) { /* catch(SYNTAX) */
+		errno = EILSEQ;
+	} { /* finally */
+		NestPoolClear(&np);
+	}
+	return !e;
+}
+/** Destructor. */
+static void Machine_(struct Machine **const pm) {
+	struct Machine *m;
+	if(!pm || !(m = *pm)) return;
+	printf("~Machine<%s>.\n", m->title);
+	LiteralsPoolForEach(&m->literals, &Literals_);
+	LiteralsPool_(&m->literals);
+	VertexPool_(&m->v);
+	MachineDigraph_(&m->graph);
+}
+/** Constructor. */
+static struct Machine *Machine(const char *const compile) {
+	struct Machine *m;
+	if(!compile) return 0;
+	if(!(m = malloc(sizeof *m))) return 0;
+	m->title = compile;
+	MachineDigraph(&m->graph);
+	VertexPool(&m->v, &MachineDigraphVertexMigrateAll, m);
+	LiteralsPool(&m->literals, &MachineDigraphEdgeMigrateAll, m);
+	if(!m_compile(m, compile)) { Machine_(&m); perror(compile); }
+	return m;
+}
+
+/** @return Error condition. */
 int main(void) {
 	unsigned seed = (unsigned)clock();
-	struct Regex *re = 0;
+	struct Machine *m = 0;
 	const char *const fn = "graphs/regex.gv";
 	FILE *fp = 0;
-	int done = 0;
+	int is_err = 0;
 	srand(seed), rand(), printf("Seed %u.\n", seed);
 	BlankDigraphTest();
 	ColourDigraphTest();
 	/* Custom. */
-	do {
-		if(!(re = Regex("hi"))) break;
-		if(!(fp = fopen(fn, "w"))) break;
-		if(!RegexOut(re, fp)) break;
-		printf("hithere <%s>; null <%s>; hihi <%s>.\n", RegexMatch(re, "hithere"), RegexMatch(re, "bye"), RegexMatch(re, "therehihi"));
-		done = 1;
-	} while(0); if(!done) {
-		perror(fn);
-	} {
-		fclose(fp);
-		Regex_(&re);
-	}
-	if(done) printf("Test success.\n\n");
-
-	return done ? EXIT_SUCCESS : EXIT_FAILURE;
+	if(!(m = Machine("hi"))
+		|| !(fp = fopen(fn, "w"))
+		|| !MachineDigraphOut(&m->graph, fp)) is_err = 1;
+	if(is_err) perror(fn);
+	fclose(fp);
+	Machine_(&m);
+	if(!is_err) printf("Test success.\n\n");
+	return is_err ? EXIT_FAILURE : EXIT_SUCCESS;
 }
