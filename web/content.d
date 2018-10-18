28e4084c283dd7a966173c0af22ce8d30f063034
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2018-10-17 21:54:14 -0400

    Updated web.

---
+++ web/content.d
@@ -0,0 +1,2 @@
+A dynamically-allocated array. If <em>POOL_STACK</em> is defined, it is
+contiguous, otherwise it has a linked-list of removed elements.
--- web/index.d
+++ web/index.d
@@ -1,3 +1,1 @@
-A storage element that supports deleting at any point. New elements
-in the array past the end cause a <em>realloc</em>. There is a
-linked-list of removed elements.
+A (in some cases) contiguous dynamically-allocated array.
