55386dae1a29ca7bd088bc69f595ea69e9d36d8d
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2018-10-17 21:53:24 -0400

    Updated web.

--- web/content.d
+++ web/content.d
@@ -1,5 +1,6 @@
 </p>
 
-<p> <img src = "List.png" width = 665 height = 512> </p>
+<p> <img src = "List.png" width = 665 height = 512 alt = "List."> </p>
 
-<p> <em>List</em> is a generic <em>C</em> linked-list.
+<p> <em>List</em> is a generic <em>C</em> linked-list with possibly
+different order permutations.
