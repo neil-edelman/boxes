fc7fe8ba28623abb3d5af288ebed1550e789dd08
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2019-12-19 18:47:45 -0500

    40000 was too much.

--- test/TestSet.c
+++ test/TestSet.c
@@ -264,7 +264,6 @@ static struct IdSetElement *id_from_pool(void *const vboats) {
 }
 
 int main(void) {
-	printf("Foo!\n");
 	{ /* Automated tests. */
 		struct BoatPool boats;
 		struct StringElementPool ses;
