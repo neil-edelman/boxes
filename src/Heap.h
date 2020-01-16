10a4c966537045ad663a4d43b9d4bb8e55a0d5a4
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2020-01-16 15:24:44 -0500

    "Updated Heap docs."

--- src/Heap.h
+++ src/Heap.h
@@ -21,7 +21,8 @@
  <typedef:<PH>Type> associated therewith; `HEAP_NAME` is required. `<PH>` is
  private, whose names are prefixed in a manner to avoid collisions; any should
  be re-defined prior to use elsewhere. Note that `HEAP_TYPE` is only used by
- reference; it can be forward-declared or left out entirely.
+ reference; it can be forward-declared or left out entirely, and the
+ implementation is [responsble for it's storage](../../Pool/).
 
  @param[HEAP_COMPARE]
  A function satisfying <typedef:<PH>Compare>. Defaults to minimum-hash using
