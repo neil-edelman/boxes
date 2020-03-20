8e9b079c2231829763ead1adcc575b626fc184fe
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2020-03-20 18:20:44 -0400

    "Added Trie."

--- src/Set.h
+++ src/Set.h
@@ -61,7 +61,8 @@
  @cf [Heap](https://github.com/neil-edelman/Heap)
  @cf [List](https://github.com/neil-edelman/List)
  @cf [Orcish](https://github.com/neil-edelman/Orcish)
- @cf [Pool](https://github.com/neil-edelman/Pool) */
+ @cf [Pool](https://github.com/neil-edelman/Pool)
+ @cf [Trie](https://github.com/neil-edelman/Trie) */
 
 #include <stdlib.h> /* realloc free */
 #include <assert.h>	/* assert */
