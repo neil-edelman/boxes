bc2cdeac10178f637132b13e48a54c874b91aaee
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2020-05-14 02:25:03 -0400

    "Split Bipredicate and Biproject up."

--- src/Array.h
+++ src/Array.h
@@ -85,8 +85,19 @@ typedef ARRAY_TYPE PT_(Type);
 /** Operates by side-effects. */
 typedef void (*PT_(Action))(T *);
 
-/** Returns a boolean given `<T>`. */
+/** Returns a boolean given read-only `<T>`. */
 typedef int (*PT_(Predicate))(const T *);
+
+/** Returns a boolean given two read-only `<T>`. */
+typedef int (*PT_(Bipredicate))(const T *, const T *);
+
+/** Returns a boolean given two `<T>`, specifying the first or second
+ argument. */
+typedef int (*PT_(Biproject))(T *, T *);
+
+/** Return false, ignore `a` and `b`. @implements <PT>Biproject */
+static int PT_(false_project)(T *const a, T *const b)
+	{ return (void)a, (void)b, 0; }
 
 /** Manages the array field `first`, which is indexed up to `size`. When
  modifying the topology of this array, it may change memory location to fit;
@@ -107,7 +118,7 @@ struct T_(Array) { T *first; size_t size, capacity; };
  @param[update_ptr] Must be in the array or null, it updates this value.
  @return Success; otherwise, `errno` will be set.
  @throws[ERANGE] Tried allocating more then can fit in `size_t` or `realloc`
- doesn't follow [IEEE Std 1003.1-2001
+ doesn't follow [POSIX
  ](https://pubs.opengroup.org/onlinepubs/009695399/functions/realloc.html).
  @throws[realloc] */
 static int PT_(update_reserve)(struct T_(Array) *const a,
@@ -189,6 +200,38 @@ static int PT_(replace)(struct T_(Array) *const a, const size_t i0,
 	return 1;
 }
 
+/** Calls `predicate` for each consecutive pair of elements in `a` and if true,
+ surjects two one according to `merge`.
+ @order \O(`a.size`) */
+static void PT_(compactify)(struct T_(Array) *const a,
+	const PT_(Bipredicate) predicate, const PT_(Biproject) merge) {
+	size_t target, from, cursor, choice, next, move;
+	const size_t last = a->size;
+	int is_first, is_last;
+	assert(a && predicate && merge);
+	for(target = from = cursor = 0; cursor < last; cursor += next) {
+		/* Bijective `[from, cursor)` is moved lazily. */
+		for(choice = 0, next = 1; cursor + next < last
+			&& predicate(a->first + cursor + choice, a->first + cursor + next);
+			next++) if(merge(a->first + choice, a->first + next)) choice = next;
+		if(next == 1) continue;
+		/* Must move injective `cursor + choice \in [cursor, cursor + next)`. */
+		is_first = !choice;
+		is_last  = (choice == next - 1);
+		move = cursor - from + is_first;
+		memmove(a->first + target, a->first + from, sizeof *a->first * move),
+			target += move;
+		if(!is_first && !is_last) memcpy(a->first + target,
+			a->first + cursor + choice, sizeof *a->first), target++;
+		from = cursor + next - is_last;
+	}
+	/* Last differed move. */
+	move = last - from;
+	memmove(a->first + target, a->first + from, sizeof *a->first * move),
+		target += move, assert(a->size >= target);
+	a->size = target;
+}
+
 /** Returns a new un-initialized datum of `a` and updates `update_ptr`. */
 static T *PT_(update_new)(struct T_(Array) *const a, T **const update_ptr) {
 	assert(a);
@@ -204,11 +247,11 @@ static T *PT_(new)(struct T_(Array) *const a) {
 
 /** Initialises `a` to idle. */
 static void PT_(array)(struct T_(Array) *const a)
-	{ assert(a); a->first = 0, a->capacity = a->size = 0; }
+	{ assert(a), a->first = 0, a->capacity = a->size = 0; }
 
 /** Destroys `a` and returns it to idle. */
 static void PT_(array_)(struct T_(Array) *const a)
-	{ assert(a); free(a->first); PT_(array)(a); }
+	{ assert(a), free(a->first), PT_(array)(a); }
 
 #ifndef ARRAY_CHILD /* <!-- !sub-type */
 
@@ -270,7 +313,7 @@ static T *T_(ArrayPop)(struct T_(Array) *const a)
  @return A new, un-initialised, element at the back of `a`, or null and `errno`
  will be set.
  @throws[ERANGE] Tried allocating more then can fit in `size_t` or `realloc`
- error and doesn't follow [IEEE Std 1003.1-2001
+ error and doesn't follow [POSIX
  ](https://pubs.opengroup.org/onlinepubs/009695399/functions/realloc.html).
  @throws[realloc] @order Amortised \O(1). @allow */
 static T *T_(ArrayNew)(struct T_(Array) *const a)
@@ -282,7 +325,7 @@ static T *T_(ArrayNew)(struct T_(Array) *const a)
  @return A new, un-initialised, element at the back of `a`, or null and `errno`
  will be set.
  @throws[ERANGE] Tried allocating more then can fit in `size_t` or `realloc`
- error and doesn't follow [IEEE Std 1003.1-2001
+ error and doesn't follow [POSIX
  ](https://pubs.opengroup.org/onlinepubs/009695399/functions/realloc.html).
  @throws[realloc] @order Amortised \O(1). @allow */
 static T *T_(ArrayUpdateNew)(struct T_(Array) *const a,
@@ -291,11 +334,11 @@ static T *T_(ArrayUpdateNew)(struct T_(Array) *const a,
 /** Ensures that `a` is `reserve` capacity beyond the elements in the array.
  @param[a] If null, returns null.
  @return The previous end of `a`, where are `reserve` elements, or null
- and `errno` will be set. Writing on this memory space is safe up to `reserve`
- elements, but one will have to increase the size manually, (see
+ and `errno` will be set. Writing on this memory space is safe on success, up
+ to `reserve` elements, but one will have to increase the size, (see
  <fn:<T>ArrayBuffer>.)
  @throws[ERANGE] Tried allocating more then can fit in `size_t` or `realloc`
- error and doesn't follow [IEEE Std 1003.1-2001
+ error and doesn't follow [POSIX
  ](https://pubs.opengroup.org/onlinepubs/009695399/functions/realloc.html).
  @throws[realloc] @allow */
 static T *T_(ArrayReserve)(struct T_(Array) *const a, const size_t reserve) {
@@ -313,7 +356,7 @@ static T *T_(ArrayReserve)(struct T_(Array) *const a, const size_t reserve) {
  @return The start of a new sub-array of `add` elements at the previous end of
  `a`, or null and `errno` will be set.
  @throws[ERANGE] Tried allocating more then can fit in `size_t` or `realloc`
- error and doesn't follow [IEEE Std 1003.1-2001
+ error and doesn't follow [POSIX
  ](https://pubs.opengroup.org/onlinepubs/009695399/functions/realloc.html). If
  <fn:<T>ArrayReserve> has been successful in reserving at least `add` elements,
  one is guaranteed success.
@@ -321,7 +364,7 @@ static T *T_(ArrayReserve)(struct T_(Array) *const a, const size_t reserve) {
 static T *T_(ArrayBuffer)(struct T_(Array) *const a, const size_t add) {
 	size_t prev_size;
 	if(!a || !add) return 0;
-	if(a->size > (size_t)-1 - add) { errno = ERANGE; return 0; }
+	if(a->size > (size_t)-1 - add) { errno = ERANGE; return 0; } /* Unlikely. */
 	if(!PT_(reserve)(a, a->size + add)) return 0;
 	prev_size = a->size;
 	a->size += add;
@@ -403,6 +446,16 @@ static void T_(ArrayKeepIf)(struct T_(Array) *const a,
 	assert((size_t)(erase - a->first) <= a->size);
 	a->size = erase - a->first;
 }
+
+/** Calls `predicate` for each consecutive pair of elements in `a` and if true,
+ surjects two one according to `merge`.
+ @param[a, predicate] If null, does nothing.
+ @param[merge] Can be null, in which case the second argument is erased.
+ @order \O(`a.size`) */
+static void T_(ArrayCompactify)(struct T_(Array) *const a,
+	const PT_(Bipredicate) predicate, const PT_(Biproject) merge)
+	{ if(a && predicate) PT_(compactify)(a, predicate,
+	merge ? merge : &PT_(false_project)); }
 
 /** Removes at either end of `a` of things that `predicate` returns true.
  @param[a, predicate] If null, does nothing.
@@ -532,24 +585,13 @@ static void PT_(unused_coda)(void);
 /** This silences unused function warnings from the pre-processor, but allows
  optimisation <http://stackoverflow.com/questions/43841780/silencing-unused-static-function-warnings-for-a-section-of-code>. */
 static void PT_(unused_set)(void) {
-	T_(Array_)(0);
-	T_(Array)(0);
-	T_(ArrayRemove)(0, 0);
-	T_(ArrayLazyRemove)(0, 0);
-	T_(ArrayClear)(0);
-	T_(ArrayPeek)(0);
-	T_(ArrayPop)(0);
-	T_(ArrayNew)(0);
-	T_(ArrayUpdateNew)(0, 0);
-	T_(ArrayReserve)(0, 0);
-	T_(ArrayBuffer)(0, 0);
-	T_(ArrayEach)(0, 0);
-	T_(ArrayIfEach)(0, 0, 0);
-	T_(ArrayAny)(0, 0);
-	T_(ArrayKeepIf)(0, 0, 0);
-	T_(ArrayTrim)(0, 0);
-	T_(ArraySplice)(0, 0, 0, 0);
-	T_(ArrayIndexSplice)(0, 0, 0, 0);
+	T_(Array_)(0), T_(Array)(0), T_(ArrayRemove)(0, 0),
+		T_(ArrayLazyRemove)(0, 0), T_(ArrayClear)(0), T_(ArrayPeek)(0),
+		T_(ArrayPop)(0), T_(ArrayNew)(0), T_(ArrayUpdateNew)(0, 0),
+		T_(ArrayReserve)(0, 0), T_(ArrayBuffer)(0, 0), T_(ArrayEach)(0, 0),
+		T_(ArrayIfEach)(0, 0, 0), T_(ArrayAny)(0, 0), T_(ArrayKeepIf)(0, 0, 0),
+		T_(ArrayCompactify)(0, 0, 0), T_(ArrayTrim)(0, 0),
+		T_(ArraySplice)(0, 0, 0, 0), T_(ArrayIndexSplice)(0, 0, 0, 0);
 #ifdef ARRAY_TO_STRING
 	T_(ArrayToString)(0);
 #endif
@@ -571,15 +613,10 @@ static void PT_(unused_coda)(void);
  used. */
 static void PT_(unused_set)(void) {
 	/* <fn:<PT>update_reserve> is integral. */
-	PT_(reserve)(0, 0);
-	PT_(range)(0, 0, 0, 0, 0);
-	PT_(replace)(0, 0, 0, 0);
-	PT_(update_new)(0, 0);
-	PT_(update_new)(0, 0);
-	PT_(new)(0);
-	PT_(array)(0);
-	PT_(array_)(0);
-	PT_(unused_coda)();
+	PT_(false_project)(0, 0), PT_(reserve)(0, 0), PT_(range)(0, 0, 0, 0, 0),
+		PT_(replace)(0, 0, 0, 0), PT_(compactify)(0, 0, 0),
+		PT_(update_new)(0, 0), PT_(new)(0), PT_(array)(0), PT_(array_)(0),
+		PT_(unused_coda)();
 }
 static void PT_(unused_coda)(void) { PT_(unused_set)(); }
 #endif /* sub-type --> */
