22cbe7d28f0e1751bcb222924f564498151bc6e2
Neil Edelman <neil dot edelman each mail dot mcgill dot ca>
2021-06-10 23:10:44 -0700

    Auto-pull, because different computers.

---
+++ autopull.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Pushes all git repositories immediately under the directory.
+# Not very safe.
+
+cd "$(dirname "$0")" || exit
+for PROJ in *; do if [[ -d "$PROJ" && -d "$PROJ/.git" && ! -L "$PROJ" ]]; then
+	(
+	cd "$PROJ" || exit
+	echo "*** Pulling $PROJ... ***"
+	git pull || true
+	)
+fi done
