c9478ec2de337401dfa04565ede41f2b6d4ba699
Neil <neil@sdf.org>
2021-06-24 20:17:44 -0700

    Wrote out docs for traits.

--- traits/to_string.h
+++ traits/to_string.h
@@ -3,12 +3,15 @@
 
  @subtitle To String Trait
 
- The inclusion must define an iterator, ITERATE, ITERATE_BOX, ITERATE_TYPE,
- ITERATE_BEGIN, and ITERATE_NEXT.
+ A sub-trait of <iterate.h>.
 
- @param[Z_]
- Function-like define macro accepting one argument and producing a valid name.
- Defines `PZ_` to be private.
+ @param[TO_STRING_]
+ A one-argument macro producing a name that is responsible for the name of the
+ to string function. Does not undefine.
+
+ @param[TO_STRING_ITERATE_]
+ A one-argument macro producing a name that is the same as has been previously
+ been called on as `ITERATE_` on <iterate.h>. This is responsible for the order.
 
  @param[TO_STRING]
  Function implementing <typedef:<PZ>to_string_fn>.
@@ -21,6 +24,7 @@
 #ifndef TO_STRING_H /* <!-- idempotent: for all in compilation unit. */
 #define TO_STRING_H
 #include <string.h>
+/* fixme: have it be extern! */
 static char to_string_buffers[4][256];
 static const unsigned to_string_buffers_no = sizeof to_string_buffers
 	/ sizeof *to_string_buffers, to_string_buffer_size
@@ -29,16 +33,10 @@ static unsigned to_string_buffer_i;
 #endif /* idempotent --> */
 
 /* Check defines. */
-#if !defined(CAT) || !defined(CAT_) || !defined(ITERATE) \
-	|| !defined(ITERATE_BOX) || !defined(ITERATE_TYPE) \
-	|| !defined(ITERATE_BEGIN) || !defined(ITERATE_NEXT)
-#error To string: CAT_? or ITERATE* are undefined.
-#endif
-#ifndef Z_
-#error To string: macro Z_ undefined.
-#endif
-#ifdef PZ_
-#error To string: PZ_ can not be defined.
+#if !defined(CAT) || !defined(CAT_) || !defined(TO_STRING_ITERATE_) \
+	|| !defined(TO_STRING_) || !defined(TO_STRING_ITERATE_) \
+	|| defined(PZ_) || defined(PI_)
+#error Unexpected preprocessor symbols.
 #endif
 #ifndef TO_STRING_LEFT
 #define TO_STRING_LEFT '('
@@ -47,33 +45,27 @@ static unsigned to_string_buffer_i;
 #define TO_STRING_RIGHT ')'
 #endif
 
-#define PZ_(thing) CAT(to_string, Z_(thing))
+#define PZ_(n) CAT(to_string, TO_STRING_(n))
+#define PI_(n) CAT(iterate, TO_STRING_ITERATE_(n))
 
-typedef ITERATE PZ_(iterator);
-typedef ITERATE_BOX PZ_(box);
-typedef ITERATE_TYPE PZ_(type);
-typedef void (*PZ_(begin_fn))(PZ_(iterator) *, const PZ_(box) *);
-typedef const PZ_(type) *(*PZ_(next_fn))(PZ_(iterator) *);
 /** Responsible for turning the first argument into a 12-`char` null-terminated
  output string. */
-typedef void (*PZ_(to_string_fn))(const PZ_(type) *, char (*)[12]);
+typedef void (*PZ_(to_string_fn))(const PI_(type) *, char (*)[12]);
 
-static const PZ_(begin_fn) PZ_(begin) = (ITERATE_BEGIN);
-static const PZ_(next_fn) PZ_(next) = (ITERATE_NEXT);
 /* Check that `TO_STRING` is a function implementing <typedef:<PZ>to_string>. */
 static const PZ_(to_string_fn) PZ_(to_string) = (TO_STRING);
 
 /** @return Print the contents of `box` in a static string buffer of 256
  bytes with limitations of only printing 4 things at a time.
  @order \Theta(1) @allow */
-static const char *Z_(to_string)(const PZ_(box) *const box) {
+static const char *TO_STRING_(to_string)(const PI_(box) *const box) {
 	const char comma = ',', space = ' ', *const ellipsis = "…",
 		left = TO_STRING_LEFT, right = TO_STRING_RIGHT;
 	const size_t ellipsis_len = strlen(ellipsis);
 	char *const buffer = to_string_buffers[to_string_buffer_i++], *b = buffer;
 	size_t advance, size;
-	const PZ_(type) *x;
-	PZ_(iterator) it;
+	const PI_(type) *x;
+	PI_(iterator) it;
 	int is_sep = 0;
 	/* Minimum size: "(" "XXXXXXXXXXX" "," "…" ")" "\0". */
 	assert(box && !(to_string_buffers_no & (to_string_buffers_no - 1))
@@ -81,9 +73,9 @@ static const char *Z_(to_string)(const PZ_(box) *const box) {
 	/* Advance the buffer for next time. */
 	to_string_buffer_i &= to_string_buffers_no - 1;
 	/* Begin iteration. */
-	PZ_(begin)(&it, box);
+	PI_(begin)(&it, box);
 	*b++ = left;
-	while(x = PZ_(next)(&it)) {
+	while(x = PI_(next)(&it)) {
 		PZ_(to_string)(x, (char (*)[12])b);
 		/* Paranoid about '\0'. */
 		for(advance = 0; *b != '\0' && advance < 11; b++, advance++);
@@ -91,7 +83,7 @@ static const char *Z_(to_string)(const PZ_(box) *const box) {
 		/* Greedy typesetting: enough for "XXXXXXXXXXX" "," "…" ")" "\0". */
 		if((size = (size_t)(b - buffer))
 			> to_string_buffer_size - 11 - 1 - ellipsis_len - 1 - 1)
-			{ if(PZ_(next)(&it)) goto ellipsis; else break; }
+			{ if(PI_(next)(&it)) goto ellipsis; else break; }
 	}
 	if(is_sep) b -= 2;
 	*b++ = right;
@@ -108,10 +100,13 @@ terminate:
 
 static void PZ_(unused_to_string_coda)(void);
 static void PZ_(unused_to_string)(void)
-	{ Z_(to_string)(0); PZ_(unused_to_string_coda)(); }
+	{ TO_STRING_(to_string)(0); PZ_(unused_to_string_coda)(); }
 static void PZ_(unused_to_string_coda)(void) { PZ_(unused_to_string)(); }
 
+#undef PI_
 #undef PZ_
+/* #undef TO_STRING_ We need this for test. */
+#undef TO_STRING_ITERATE_
 #undef TO_STRING
 #undef TO_STRING_LEFT
 #undef TO_STRING_RIGHT
