1798db36acb2c9e6d6c33a5a3ca61393176e0c8b
Neil <neil@sdf.org>
2021-10-10 21:37:41 -0700

    Was getting tired of having to do this again and again. Not tested.

---
+++ autoclone.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Downloads all the boxes. One can download any
+# number of them, but I found this to be frequent
+# enough to warrant it's own script.
+
+set -e
+cd "$(dirname "$0")" || exit
+[[ ! -e "array" ]] && git clone https://github.com/neil-edelman/array
+[[ ! -e "bmp" ]] && git clone https://github.com/neil-edelman/bmp
+[[ ! -e "heap" ]] && git clone https://github.com/neil-edelman/heap
+[[ ! -e "list" ]] && git clone https://github.com/neil-edelman/list
+[[ ! -e "orcish" ]] && git clone https://github.com/neil-edelman/orcish
+[[ ! -e "pool" ]] && git clone https://github.com/neil-edelman/pool
+[[ ! -e "set" ]] && git clone https://github.com/neil-edelman/set
+[[ ! -e "trie" ]] && git clone https://github.com/neil-edelman/trie
