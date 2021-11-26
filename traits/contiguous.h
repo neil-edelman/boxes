15804a41e24e138f317f10950653062335d70b47
Neil <neil@sdf.org>
2021-11-25 20:22:04 -0800

    Updated array with traits.

--- traits/contiguous.h
+++ traits/contiguous.h
@@ -7,33 +7,41 @@
  stored in array `data` up to `size_t size` such that `memcpy` will work.
  `<BOX>append` function defined.
 
- @param[Z_]
+ @param[CG_]
  A one-argument macro producing a name that is responsible for the name of the
- functions. Should be something like `Z_(x) -> foo_widget_x`.
+ functions. Should be something like `CG_(x) -> foo_widget_x`.
 
  @std C89 */
 
-#include <limits.h>
-
-#if !defined(CAT) || !defined(CAT_) || !defined(BOX_) \
-	|| !defined(BOX_CONTAINER) || !defined(BOX_CONTENTS) || !defined(Z_)
+#if !defined(BOX_) || !defined(BOX_CONTAINER) || !defined(BOX_CONTENTS) \
+	|| !defined(CG_)
 #error Unexpected preprocessor symbols.
 #endif
 
-#define PZ_(n) CAT(function, Z_(n))
+#ifndef CONTIGUOUS_H /* <!-- idempotent */
+#define CONTIGUOUS_H
+#include <limits.h>
+/* <Kernighan and Ritchie, 1988, p. 231>. */
+#if defined(CONTIGUOUS_CAT_) || defined(CONTIGUOUS_CAT) || defined(PCG_)
+#error Unexpected defines.
+#endif
+#define CONTIGUOUS_CAT_(n, m) n ## _ ## m
+#define CONTIGUOUS_CAT(n, m) CONTIGUOUS_CAT_(n, m)
+#define PCG_(n) CONTIGUOUS_CAT(contiguous, CG_(n))
+#endif /* idempotent --> */
 
-typedef BOX_CONTAINER PZ_(box);
-typedef BOX_CONTENTS PZ_(type);
+typedef BOX_CONTAINER PCG_(box);
+typedef BOX_CONTENTS PCG_(type);
 
 /** Operates by side-effects on <typedef:<PZ>type>. */
-typedef void (*PZ_(action_fn))(PZ_(type) *);
+typedef void (*PCG_(action_fn))(PCG_(type) *);
 
 /** Returns a boolean given read-only <typedef:<PZ>type>. */
-typedef int (*PZ_(predicate_fn))(const PZ_(type) *);
+typedef int (*PCG_(predicate_fn))(const PCG_(type) *);
 
 /** @return Converts `i` to an index in `box` from [0, `a.size`]. Negative
  values are implicitly plus `box.size`. @order \Theta(1) @allow */
-static size_t Z_(clip)(const PZ_(box) *const box, const long i) {
+static size_t CG_(clip)(const PCG_(box) *const box, const long i) {
 	/* `SIZE_MAX` is `C99`; assumes two's-compliment. */
 	assert(box && (size_t)-1 >= (size_t)LONG_MAX
 		&& (unsigned long)((size_t)-1) >= LONG_MAX);
@@ -46,10 +54,10 @@ static size_t Z_(clip)(const PZ_(box) *const box, const long i) {
  elements to `a`. `a` and `b` can not be the same but `b` can be null, (in
  which case, it does nothing.)
  @order \O(`b.size` \times `copy`) @throws[ERANGE, realloc] @allow */
-static int Z_(copy_if)(PZ_(box) *const a, const PZ_(predicate_fn) copy,
-	const PZ_(box) *const b) {
-	PZ_(type) *i, *fresh;
-	const PZ_(type) *end, *rise = 0;
+static int CG_(copy_if)(PCG_(box) *const a, const PCG_(predicate_fn) copy,
+	const PCG_(box) *const b) {
+	PCG_(type) *i, *fresh;
+	const PCG_(type) *end, *rise = 0;
 	size_t add;
 	int difcpy = 0;
 	assert(a && copy && a != b);
@@ -77,10 +85,10 @@ static int Z_(copy_if)(PZ_(box) *const a, const PZ_(predicate_fn) copy,
 /** For all elements of `box`, calls `keep`, and if false, lazy deletes that
  item, calling `destruct` if not-null.
  @order \O(`a.size` \times `keep` \times `destruct`) @allow */
-static void Z_(keep_if)(PZ_(box) *const box,
-	const PZ_(predicate_fn) keep, const PZ_(action_fn) destruct) {
-	PZ_(type) *erase = 0, *t;
-	const PZ_(type) *retain = 0, *end;
+static void CG_(keep_if)(PCG_(box) *const box,
+	const PCG_(predicate_fn) keep, const PCG_(action_fn) destruct) {
+	PCG_(type) *erase = 0, *t;
+	const PCG_(type) *retain = 0, *end;
 	int keep0 = 1, keep1 = 0;
 	assert(box && keep);
 	for(t = box->data, end = t + box->size; t < end; keep0 = keep1, t++) {
@@ -113,7 +121,8 @@ static void Z_(keep_if)(PZ_(box) *const box,
 
 /** Removes at either end of `box` of things that `predicate` returns true.
  @order \O(`box.size` \times `predicate`) @allow @fixme */
-static void Z_(trim)(PZ_(box) *const box, const PZ_(predicate_fn) predicate) {
+static void CG_(trim)(PCG_(box) *const box,
+	const PCG_(predicate_fn) predicate) {
 	size_t i;
 	assert(box && predicate);
 	while(box->size && predicate(box->data + box->size - 1)) box->size--;
@@ -126,8 +135,8 @@ static void Z_(trim)(PZ_(box) *const box, const PZ_(predicate_fn) predicate) {
 /** Iterates through `box` and calls `action` on all the elements. The topology
  of the list should not change while in this function.
  @order \O(`box.size` \times `action`) @allow */
-static void Z_(each)(PZ_(box) *const box, const PZ_(action_fn) action) {
-	PZ_(type) *i, *end;
+static void CG_(each)(PCG_(box) *const box, const PCG_(action_fn) action) {
+	PCG_(type) *i, *end;
 	assert(box && action);
 	for(i = box->data, end = i + box->size; i < end; i++) action(i);
 }
@@ -136,9 +145,9 @@ static void Z_(each)(PZ_(box) *const box, const PZ_(action_fn) action) {
  `predicate` returns true. The topology of the list should not change while in
  this function. @order \O(`box.size` \times `predicate` \times `action`)
  @allow */
-static void Z_(if_each)(PZ_(box) *const box,
-	const PZ_(predicate_fn) predicate, const PZ_(action_fn) action) {
-	PZ_(type) *i, *end;
+static void CG_(if_each)(PCG_(box) *const box,
+	const PCG_(predicate_fn) predicate, const PCG_(action_fn) action) {
+	PCG_(type) *i, *end;
 	assert(box && predicate && action);
 	for(i = box->data, end = i + box->size; i < end; i++)
 		if(predicate(i)) action(i);
@@ -148,21 +157,20 @@ static void Z_(if_each)(PZ_(box) *const box,
  until it returns true.
  @return The first `predicate` that returned true, or, if the statement is
  false on all, null. @order \O(`box.size` \times `predicate`) @allow */
-static const PZ_(type) *Z_(any)(const PZ_(box) *const box,
-	const PZ_(predicate_fn) predicate) {
-	PZ_(type) *i, *end;
+static const PCG_(type) *CG_(any)(const PCG_(box) *const box,
+	const PCG_(predicate_fn) predicate) {
+	PCG_(type) *i, *end;
 	assert(box && predicate);
 	for(i = box->data, end = i + box->size; i < end; i++)
 		if(predicate(i)) return i;
 	return 0;
 }
 
-static void PZ_(unused_function_coda)(void);
-static void PZ_(unused_function)(void) {
-	Z_(clip)(0, 0); Z_(copy_if)(0, 0, 0); Z_(keep_if)(0, 0, 0); Z_(trim)(0, 0);
-	Z_(each)(0, 0); Z_(if_each)(0, 0, 0); Z_(any)(0, 0);
-	PZ_(unused_function_coda)(); }
-static void PZ_(unused_function_coda)(void) { PZ_(unused_function)(); }
+static void PCG_(unused_function_coda)(void);
+static void PCG_(unused_function)(void) {
+	CG_(clip)(0, 0); CG_(copy_if)(0, 0, 0); CG_(keep_if)(0, 0, 0); CG_(trim)(0, 0);
+	CG_(each)(0, 0); CG_(if_each)(0, 0, 0); CG_(any)(0, 0);
+	PCG_(unused_function_coda)(); }
+static void PCG_(unused_function_coda)(void) { PCG_(unused_function)(); }
 
-#undef PZ_
 #undef Z_
