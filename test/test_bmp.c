70fc5841e9e5200124ce12427ebb313bc3134b9c
Neil <neil@sdf.org>
2022-10-05 12:41:46 -0700

    Fixed when bits is a multiple of sizeof unsigned.

--- test/test_bmp.c
+++ test/test_bmp.c
@@ -16,10 +16,22 @@
 #define BMP_TEST
 #include "../src/bmp.h"
 
+#define BMP_NAME d
+#define BMP_BITS 32
+#define BMP_TEST
+#include "../src/bmp.h"
+
+#define BMP_NAME e
+#define BMP_BITS 256
+#define BMP_TEST
+#include "../src/bmp.h"
+
 /** Entry point. @return Either EXIT_SUCCESS or EXIT_FAILURE. */
 int main(void) {
 	b_bmp_tests();
 	a_bmp_tests();
 	c_bmp_tests();
+	d_bmp_tests();
+	e_bmp_tests();
 	return EXIT_SUCCESS;
 }
