790095c8c042904db0f1a12a68ac94872dff194f
Neil <neil@sdf.org>
2025-02-05 17:59:17 -0800

    Removed extraneous comments.

--- test/header_deque.h
+++ test/header_deque.h
@@ -1,7 +1,3 @@
-/* This header is for potentially multiple files, but only one can contain the
- definition. All others just have declarations by `DEQUE_DECLARE_ONLY`. It's
- more convenient to have implicit declarations unless otherwise stated. We make
- up a preprocessor variable that flips them. */
 #ifdef DEFINE
 #	undef DEFINE
 #else
